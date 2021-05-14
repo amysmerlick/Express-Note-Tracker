ae259b72262ebab53bfd166d4483adc313da723c
Amy Smerlick <77814900+amysmerlick@users.noreply.github.com>
2021-05-14 16:21:03 -0400

    working on heroku port

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const apiRoutes = require('./routes/apiroutes.js')
-const htmlRoutes = require('./routes/htmlroutes.js')
+const apiRoutes = require('./routes/apiRoutes.js')
+const htmlRoutes = require('./routes/htmlRoutes.js')
 
 
 const app = express();
 
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 
 app.use(express.urlencoded({ extended: true }));
